e8fe9f7831135f779224b09363275610664af1ac
Mohiminul Islam Ahad <122167069+ahad30@users.noreply.github.com>
2024-04-21 16:04:58 +0600

    component setting complete

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,11 +1,19 @@
 import {
   createBrowserRouter,
 } from "react-router-dom";
+import MainLayout from "../components/Layout/MainLayout";
+import Home from "../Pages/Home/Home";
 
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
+    children: [
+        {
+            path: "",
+            element: <Home></Home>
+        }
+    ]
   },
 ]);
