8c7de8a403133f27a8259da411e9159dd534c1b8
unknown <naimurk781@gmail.com>
2024-04-23 10:55:21 +0600

    nabar done

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -1,5 +1,12 @@
 import { Link } from "react-router-dom";
 import { MdOutlineArrowRightAlt } from "react-icons/md";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoLogoFacebook } from "react-icons/io5";
+import { FaTwitter , FaLinkedin , FaBehance , FaYoutube} from "react-icons/fa";
+
+
+
+
 
 const Navbar = () => {
   return (
@@ -11,10 +18,34 @@ const Navbar = () => {
             Welcome to our international shop! Enjoy free shipping on orders
             $100 & up
           </p>
-          <p className="text-[#FF0935] flex items-center gap-x-1 border-b border-[#FF0935] text-xs font-bold"><Link to={"#"}>Shop now</Link> <MdOutlineArrowRightAlt size={17}></MdOutlineArrowRightAlt></p>
+          <p className="text-[#FF0935] flex items-center gap-x-1 border-b border-[#FF0935] text-xs font-bold">
+            <Link to={"#"}>Shop now</Link>{" "}
+            <MdOutlineArrowRightAlt size={17}></MdOutlineArrowRightAlt>
+          </p>
         </div>
         {/* icon part */}
-        <div></div>
+        <div className="flex items-center gap-x-12 justify-between">
+          {/* language */}
+          <div className="font-bold text-base flex items-center gap-x-12 text-white">
+            <div className="flex gap-x-1 items-center">
+              <p>USD</p>
+              <IoIosArrowDown size={20}></IoIosArrowDown>
+            </div>
+            <div className="flex gap-x-1 items-center">
+              <p>English</p>
+              <IoIosArrowDown size={20}></IoIosArrowDown>
+            </div>
+          </div>
+          {/* icon */}
+          <div className="text-white flex items-center gap-x-3">
+            <IoLogoFacebook size={20} className=""></IoLogoFacebook>
+            <FaTwitter size={18}></FaTwitter>
+            <FaBehance size={18}></FaBehance>
+            <FaYoutube size={18}></FaYoutube>
+            <FaLinkedin size={18}></FaLinkedin>
+            
+          </div>
+        </div>
       </div>
     </div>
   );
