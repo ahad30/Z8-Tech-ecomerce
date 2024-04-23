25ad5d87da5e9d4ec3ade2371d724efeaf53bf06
unknown <naimurk781@gmail.com>
2024-04-23 15:44:50 +0600

    photo add

--- src/Pages/Home/Product/Product.tsx
+++ src/Pages/Home/Product/Product.tsx
@@ -1,3 +1,15 @@
+import img1 from "../../../assets/Product/product-01.png"
+import img2 from "../../../assets/Product/product-02.png"
+import img3 from "../../../assets/Product/product-03.png"
+import img4 from "../../../assets/Product/product-04.png"
+import img5 from "../../../assets/Product/product-05.png"
+import img6 from "../../../assets/Product/product-06.png"
+import img7 from "../../../assets/Product/product-07.png"
+import img8 from "../../../assets/Product/product-08.png"
+import img9 from "../../../assets/Product/product-09.png"
+import img10 from "../../../assets/Product/product-10.png"
+import img11 from "../../../assets/Product/product-11.png"
+import img12 from "../../../assets/Product/product-12.png"
 
 
 const Product = () => {
