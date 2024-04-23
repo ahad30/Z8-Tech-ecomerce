98efe1b1c91bcc6cd45b7fd1441df57c5529c0a7
Ummepinki <salmaumme888@gmail.com>
2024-04-23 17:19:08 +0600

    footer  icon added

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,18 +1,17 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../Shared/Navbar"
-import Footer from "../Shared/Footer"
-
+import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
+import Footer from "../Shared/Footer";
 
 const MainLayout = () => {
   return (
     <div className="">
-        <Navbar></Navbar>
-       <div className="max-w-6xl  mx-auto">
-       <Outlet></Outlet>
-       </div>
-        <Footer></Footer>
+      <Navbar></Navbar>
+      <div className="max-w-6xl  mx-auto">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <div>
       <div className="bg-[#600451] h-[351px] flex mt-10 ">
-        <div className="px-14">
+        <div className="px-20">
           <p className="text-2xl font-semibold text-white pt-12">Z8 eComerce</p>
           <p className="w-[320px] pt-4 text-white text-[16px]">
             Elegant pink origami design three dimensional view and decoration
@@ -18,7 +18,8 @@ const Footer = () => {
           <p className="pt-5 text-white">Enter email address</p>
           <p className="border w-[270px] h-[40px] p-2 text-white mt-3">Email</p>
         </div>
-        <div className="pl-24 text-white text-sm">
+        <div></div>
+        <div className="pl-32 text-white text-sm">
           <p className="pt-14 underline text-[16px] font-semibold">
             Information
           </p>
@@ -40,32 +41,32 @@ const Footer = () => {
             <li className="mb-3 mt-4">Terms & Condition</li>
           </ul>
         </div>
-        <div className="pl-8 text-white text-sm">
+        <div className="pl-12 text-white text-sm">
           <p className="pt-14 underline text-[16px]">Social Network</p>
           <ul className="text-[12px]">
-            <li className="mb-3 mt-4 flex justify-center items-center gap-2">
+            <li className="mb-3 mt-4 flex gap-2">
               <IoLogoFacebook className="w-[18px] h-[18px]" />
               Facebook
             </li>
-            <li className="mb-3 mt-4 flex justify-center items-center gap-2 mr-2">
+            <li className="mb-3 mt-4 flex  gap-2">
               <FaDribbble className="w-[18px] h-[18px]" />
               Dribble
             </li>
-            <li className="mb-3 mt-4 flex justify-center items-center gap-2 mr-2">
+            <li className="mb-3 mt-4 flex  gap-2">
               <FaTwitter className="w-[18px] h-[18px]" />
               Twitter
             </li>
-            <li className="mb-3 mt-4 flex justify-center items-center gap-2 mr-2">
+            <li className="mb-3 mt-4 flex gap-2">
               <SiBehance className="w-[18px] h-[18px]" />
               Behance
             </li>
-            <li className="mb-3 mt-4 flex justify-center items-center gap-2 mr-2">
+            <li className="mb-3 mt-4 flex  gap-2">
               <FaYoutube className="w-[18px] h-[18px]" />
               Youtube
             </li>
           </ul>
         </div>
-        <div className="pl-10 text-white">
+        <div className="pl-12 text-white">
           <p className="pt-14 underline mb-3">Get Newsletter</p>
           <p className="w-[226px] h-[46px] mt-3 text-[16px]">
             Get on the list and get 10% off your first order!
